32a365304ddd9909f39f18c20a767ee44b7333c9
priyamsodhiya <priyamsodhiya.1998@gmail.com>
2018-12-15 11:19:33 +0530

    With data files

---
+++ qwerty/ipython.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 25 13:09:36 2018
+
+@author: prodi
+"""
+import pandas as pd
+
+from blueshift.utils.calendars import TradingCalendar
+from blueshift.utils.brokers.backtest import BackTest
+from blueshift.algorithm.algorithm import TradingAlgorithm
+from blueshift.algorithm.context import AlgoContext
+
+trading_calendar = TradingCalendar(name='NSE-Calendar', 
+                                   tz='Asia/Calcutta',
+                                   opens=(9,15,0),
+                                   closes=(15,30,0),
+                                   weekends=(5,6))
+
+
+backtester = BackTest(name='Blueshift', 
+                      start_date=pd.Timestamp("2010-01-04"),
+                      end_date=pd.Timestamp("2018-01-04"),
+                      frequency=1, trading_calendar=trading_calendar,
+                      initial_capital=10000)
+
+context = AlgoContext(name='Blueshift', broker=backtester)
+
+algo = TradingAlgorithm(name="Blueshift", context=context, 
+                        algo="user_algo.py")
+
+perf = algo.back_test_run()
+
